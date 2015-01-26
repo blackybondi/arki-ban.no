457a511a29ba047c3f05d66dbacf65981bbb4b89
murtada alamir <blackbondi@msn.com>
2015-01-26 15:33:26 +0100

    Initial check-in
    Grundleggende WordPress: index.php,header.php,footer.php og style.css

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<?php 
+// http://codex.wordpress.org/Function_Reference/wp_head
+wp_head(); 
+?>
+</head>
+
+<body <?php body_class(); ?>>
+	<header>	
+	</header>
